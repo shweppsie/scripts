805eacc8833a1d3f9e3a52a632a2a8a95421500e
Nathan Overall <git@shweppsie.com>
2012-02-20 19:09:33 +1300

    Improved interface and code efficiency.

--- get_artist_images.sh
+++ get_artist_images.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+function usage {
+	echo ""
+	echo "This script will try to find artist images from"
+	echo "last.fm using the titles of the folder in the"
+	echo "specified directory. It will then place these"
+	echo "files in each folder title \"folder.jpg\"."
+	exit 1
+}
+
 if [ $# -ne 1 ]; then
-	echo "Usage"
+	echo "Usage: $0 directory"
+	usage
+fi
+
+if [ -d "$1" ]; then
+	cd "$1"
+else
+	echo "Folder \"$1\" does not exist"
 	exit 1
 fi
 
-cd "$1"
-
 for artist in *; do
-	echo "Fetching artist art for '$artist'"
 	if [ -d "$artist" ]; then
-		wget --no-verbose -O "$artist/artist.png" "`/src/albumidentify/src/renamealbum/lastfm.py artist "$artist" 'image' | tail -n 1`"
+		if [ ! -e "$artist/folder.jpg" ]; then
+			wget --no-verbose -O "$artist/folder.jpg" "`/src/albumidentify/src/renamealbum/lastfm.py artist "$artist" "image" | tail -n 1`" &> /dev/null
+			if [ $? -ne 0 ]; then
+				echo "`date +'%d/%m/%y:%H:%M:%S' $0:` Error processing \"$artist\""
+			fi
+		fi
 	fi
-	echo ""
 done
