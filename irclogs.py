b6990a2e59668f504ed1fad4f461160edbd74229
Nathan Overall <git@shweppsie.com>
2012-04-13 03:14:16 +1200

    rearrange internal logic
     - fix a few bugs
     - rearrange code to make it flow more logically

--- irclogs.py
+++ irclogs.py
@@ -4,20 +4,20 @@ import os, gzip, re, hashlib, sys
 from time import strftime, strptime, mktime
 
 # start and end of each html file
-start = """<html><head><style type="text/css">
-		html {
-			color: white;
-			background-color: black;
-		}
-		p {
-			margin:0px;
-			font-family: Fixed, monospace;
-		}
-		A:link { color: #99CCFF; }
-		A:visited { color: #993300; }
-		A.geek:link { color: #FFFF99; }
-</style></head><body>"""
-end = """</body></html>"""
+start = """
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>IRC Logs</title>
+		<link href="/log.css" rel="stylesheet" type="text/css">
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	</head>
+	<body>
+"""
+end = """
+	</body>
+</html>
+"""
 
 UPDATE_FILE=os.path.expanduser("~/.irclog.lastupdate")
 
@@ -40,17 +40,20 @@ class LogEntry:
 		self.host = host
 		self.text = text
 
-	def __str__(self):
-		return "%s %s: <%s> %s" % (self.get_time(),self.channel,self.user,self.text)
+	def __repr__(self):
+		return self.__str__()
 
-	def get_day(self):
-		return self.time[2]
+	def __str__(self):
+		return '"%s %s: <%s> %s"' % (self.get_time(),self.channel,self.user,self.text)
 
 	def get_channel(self,nohash=False):
 		if nohash and self.channel.startswith("#"):
 			return self.channel[1:]
 		else:
 			return self.channel
+
+	def get_ticks(self):
+		return mktime(self.time)
 
 	def get_time(self):
 		return strftime("%a %d %b %Y %X",self.time)
@@ -72,6 +75,9 @@ class LogEntry:
 			return self.text
 		else:
 			text = self.text
+			replace = [("<","&lt;"),(">","&gt;"),("&","&amp;")]
+			for i in replace:
+				text = text.replace(i[0],i[1])
 			urls = url.findall(text)
 			for link in urls:
 				if link.startswith('http://geek.cn/') and self.user == 'tinybot':
@@ -79,6 +85,28 @@ class LogEntry:
 				else:
 					text = text.replace(link,'<a href="%s">%s</a>' % (link,link))
 			return text
+
+################################################
+#                   FUNCTIONS                  #
+################################################
+
+def get_channels(log):
+	channels = {}
+	for line in log:
+		chan = line.get_channel(nohash=True)
+		if chan not in channels:
+			channels[chan] = []
+		channels[chan].append(line)
+	return channels
+
+def get_days(log):
+	days = {}
+	for line in log:
+		date = line.get_date()
+		if date not in days:
+			days[date] = []
+		days[date].append(line)
+	return days
 
 ################################################
 #                    SCRIPT                    #
@@ -106,7 +134,14 @@ for path in INDIR:
 			files.append(filename)
 
 # dictionary for storing processed log items
-log = {}
+log = []
+
+# We've already process everything up to this timestamp
+last_update = 0
+if os.path.exists(UPDATE_FILE):
+	output = open(UPDATE_FILE,'r')
+	last_update = float(output.read())
+	output.close()
 
 # process log files
 for path in files:
@@ -133,73 +168,51 @@ for path in files:
 
 					item = LogEntry(time,channel,user,host,text)
 					
-					log[mktime(time)] = item
+					if item.get_ticks() > last_update:
+						log.append(item)
 	finally:
 		f.close()
 
-# we use the time in seconds since the epoch
-# to index the dictionary. Therefore sorting
-# the keys, sorts all the log items we've
-# read in.
-keys = log.keys()
-keys.sort()
 
-# varibles used to keep track of stuff as
-# we process each log item
-count = 0
-cur_day = 0
-out_file = None
+# get_ticks returns the time in seconds
+# since the epoch. We then sort the list
+# based on this.
+log = sorted(log, key=lambda line: line.get_ticks())
 
-# what was the timestamp of the last item we processed
-last_entry = 0
-if os.path.exists(UPDATE_FILE):
-	last_entry = float(open(UPDATE_FILE,'r').read())
+# break the log into channels
+channels = get_channels(log)
 
-# process items
-for i in keys:
-	# ignore stuff we've already processed
-	if i <= last_entry:
-		continue
+# break the log into days
+for chan in channels.keys():
+	channels[chan] = get_days(channels[chan])
 
-	item = log[i]
-	item_day = item.get_day()
-	
-	# create a new file if we've changed day
-	if cur_day != item_day:
-		cur_day = item_day
-		
-		# end the last file
-		if out_file is not None:
-			out_file.write(end)
-			out_file.close()
-		
-		# create directory if necessary
-		chandir = os.path.join(OUTDIR,item.get_channel(nohash=True))
-		if not os.path.isdir(chandir):
-			os.makedirs(chandir)
+for channel in channels:
+	# create directory if necessary
+	chandir = os.path.join(OUTDIR,channel)
+	if not os.path.isdir(chandir):
+		os.makedirs(chandir)
 
-		filename = os.path.join(chandir,item.get_date())+'.html'
-		
+	for day in channels[channel]:
+		filename = os.path.join(chandir,day)+'.html'
+
 		# append or create file
-		out_file = open(filename,'a')
-		out_file.write(start)
-	
-	# actually write out the log entry
-	out_file.write("<p>%s <span style=\"color: %s\">&lt%s&gt</span> %s</p>" % (item.get_time(),item.get_user_color(),item.get_user(),item.get_text()))
-	count += 1
+		if not os.path.exists(filename):
+			out_file = open(filename,'w')
+			out_file.write(start)
+		else:
+			out_file = open(filename,'a')
+			#seek back before the end text
+			out_file.seek((len(end))*(-1), os.SEEK_END)
 
-	# remember the last item
-	last_entry = i
-
-# don't update files if we did nothing
-if out_file is not None:
-	# end the last file
-	out_file.write(end)
-	out_file.close()
+		for item in channels[channel][day]:
+			out_file.write("<p>%s <span style=\"color: %s\">&lt;%s&gt;</span> %s</p>" % (item.get_time(),item.get_user_color(),item.get_user(),item.get_text()))
+			last_update = item.get_ticks()
+		
+		#write the end to the file
+		out_file.write(end)
 
 # save the timestamp from the last item
-output = open(UPDATE_FILE, 'w')
-output.write(str(i))
+output = open(UPDATE_FILE,'w')
+output.write(str(last_update))
 output.close()
 
-#print "Processed %s lines" % count
