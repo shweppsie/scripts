f7f431a9ebc117c548ee94bf5d2414f38b63336e
Nathan Overall <git@shweppsie.com>
2012-05-05 01:11:13 +1200

    Escape any options before providing the strings

--- rename_replace.sh
+++ rename_replace.sh
@@ -4,6 +4,6 @@ ls | egrep -- "${1}"| while read line
 do
 	newname="`echo "${line}" | sed -- "s/${1}/${2}/g"`"
 	echo "${line} -> ${newname}"
-	mv "${line}" "${newname}"
+	mv -- "${line}" "${newname}"
 done
 
