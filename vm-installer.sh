ba80fed4829794dabf97221228b3371d87f75a5b
Nathan Overall <git@shweppsie.com>
2012-05-05 01:13:14 +1200

    Make script usable from the command line

--- vm-installer.sh
+++ vm-installer.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-NAME=hermes
-MEMORY=128
+if [ $# -ne 3 ]; then
+	echo "Usage: $0 NAME MEMORY SIZE"
+fi
+
+NAME=$1
+MEMORY=$2 #size in MB
+SIZE=$3 #size in GB
 
 virt-install \
---name=$NAME \
---ram=$MEMORY \
+--name=${NAME} \
+--ram=${MEMORY} \
 --location=http://ftp.nz.debian.org/debian/dists/stable/main/installer-amd64/ \
 --os-type=linux \
 --os-variant=debiansqueeze \
---disk=/img/$NAME.img,device=disk,bus=virtio,size=8,sparse=true,format=raw \
+--disk=/img/${NAME}.img,device=disk,bus=virtio,size=${SIZE},sparse=true,format=raw \
 --network=bridge=lan,model=virtio \
 --extra-args=console=ttyS0,115200
 
