0872334f4488814802dae1f4b0aa42bb7e857319
Nathan Overall <git@shweppsie.com>
2012-05-05 01:10:47 +1200

    Replace the every occurance of the string

--- rename_replace.sh
+++ rename_replace.sh
@@ -2,7 +2,7 @@
 
 ls | egrep -- "${1}"| while read line
 do
-	newname="`echo "${line}" | sed -- "s/${1}/${2}/"`"
+	newname="`echo "${line}" | sed -- "s/${1}/${2}/g"`"
 	echo "${line} -> ${newname}"
 	mv "${line}" "${newname}"
 done
