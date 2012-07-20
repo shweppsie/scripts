faa7cf44bd59716f32b6562fc834cb6d38795e2b
Nathan Overall <git@shweppsie.com>
2012-07-21 03:20:09 +1200

    Improve how we do logging

--- bin/rsync.py
+++ bin/rsync.py
@@ -4,12 +4,22 @@ import sys, time
 import subprocess
 import argparse
 
-def log(text, start=False):
-	global log
-	if start:
-		logger.write('\n')
-	logger.write('%s: %s\n' % (time.asctime(), text))
-	logger.flush()
+class Log:
+	def __init__(self, logfile):
+		self.fileno = sys.stdout.fileno
+		if logfile != None:
+			self.logfile = open(logfile,'a')
+		else:
+			self.logfile = sys.stdout
+
+	def write(self, text):
+		output = ('%s: %s\n' % (time.asctime(), text))
+		self.logfile.write(output)
+		self.logfile.flush()
+
+	def close(self):
+		self.logfile.flush()
+		self.logfile.close()
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Transfer files')
@@ -25,21 +35,19 @@ args = parse_args()
 
 #first arg is source
 source = args.source
+logfile = args.logfile
 
-# open logfile
-if args.stdout:
-	logger = sys.stdout
-else:
-	logger = open('/var/log/downloads/downloads.log','a')
 
 stop_hour = int(args.stop_time)
+# prepare logging
+log = Log(logfile)
 
 if time.localtime().tm_hour >= stop_hour:
-	log("Not Starting. Already after stop time of %d:00." % stop_hour)
+	log.write("Not Starting. Already after stop time of %d:00." % stop_hour)
 	exit(1)
 else:
-	log('Starting Download',start=True)
 	log('Using a stop time of %d:00' % stop_hour)
+	log.write('\nStarting Download')
 
 DOWNLOADS_FILE = '/stuff/shared/downloads/donbot/downloads.txt'
 
@@ -52,17 +60,17 @@ for i in open(DOWNLOADS_FILE).read().split('\n'):
 		download_files.append('%s/%s' % (source, i))
 
 if len(download_files) == 0:
-	log("Nothing to download")
+	log.write("Nothing to download")
 	exit(0)
 
 downloads.extend(download_files)
 
 downloads.append(args.destination)
 
-log(repr(downloads))
+log.write(repr(downloads))
 
 for i in xrange(5):
-	p = subprocess.Popen(downloads,stderr=logger,stdout=logger)
+	p = subprocess.Popen(downloads,stderr=log.logfile,stdout=log.logfile)
 	# program has terminated?
 	while p.poll() == None:
 		# quit if it's after 7am
@@ -71,24 +79,26 @@ for i in xrange(5):
 			time.sleep(10)
 			if p.poll:
 				p.kill()
-			log("Stopping...7am limit reached")
+			log.write("Stopping...7am limit reached")
 			exit(1)
 		#re-check in a minute
 		time.sleep(60)
 	if p.returncode == 0:
-		log("Downloads Completed Successfully")
+		log.write("Downloads Completed Successfully")
 		exit(0)
 	else:
 		# wait 5 minutes and try again
-		log("Application Quit with Error Code")
-		log("Waiting 5 minutes and trying again")
+		log.write("Application Quit with Error Code")
+		log.write("Waiting 5 minutes and trying again")
 
 		time.sleep(5*60)
 
-		log("Retrying")
+		log.write("Retrying")
 
-log("Giving up after 5 attempts")
-print "%s: Giving up after 5 attempts" % time.asctime()
+log.write("Giving up after 5 attempts")
+sys.stderr.write("%s: Giving up after 5 attempts" % time.asctime())
+
+log.close()
 
 exit(1)
 
