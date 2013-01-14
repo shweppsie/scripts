3644172abcb67f4a257dd80578e9cd340b1e4831
Nathan Overall <git@shweppsie.com>
2013-01-14 21:02:07 +1300

    [validator] Fix tabbing, compile regexes, tidy code

--- video/validator.py
+++ video/validator.py
@@ -6,81 +6,77 @@ import sys,os,re
 #TODO: detect empty folders
 #TODO: detect missing episodes from a Season
 
-VIDEO=['.avi','.mp4','.mkv','.mov','.wmv','.divx','.m4v','.mpg','.iso','.flv']
-THUMB=['.jpg','.png','.srt']
-SPECIAL=['.mp3']
-
-def status(reason,path):
-	print "%s: %s" % (reason,path)
+season = re.compile(r'(Specials|Season (0[1-9]|[1-9][0-9]))')
+tv = re.compile(r'(.*) - ([0-9]{1,2})(x([0-9]{2,3}))+( - .*)?\.[^.]+$')
 
 def failed(path, reason):
-	status("Failed (%s)" % reason,path)
-	return 1
+    print "Failed (%s): %s" % (reason,path)
+    return 1
 
-def passed(path):
-	status("Passed",path)
-	return 0
+def processTV(path):
+    fullpath = path
+    path, filename = os.path.split(path)
+    path, folder_season = os.path.split(path)
+    path, folder_seriesname = os.path.split(path)
 
-def processMovie(path, debug=False):
-	return 0
+    # check for leader 'The'
+    if folder_seriesname.startswith('The '):
+        return failed(fullpath,
+                "The should be moved to the end of names")
 
-def processTV(path,debug=False):
-	fullpath = path
-	path, filename = os.path.split(path)
-	path, seasondir = os.path.split(path)
-	path, seriesname = os.path.split(path)
+    if folder_seriesname.endswith(', The'):
+        folder_seriesname = "The %s" % folder_seriesname[:-5]
 
-	if seriesname.startswith('The '):
-		return failed(fullpath,"Series does not follow spec")
+    # check name on season folder
+    res = season.match(folder_season)
+    if not res:
+        return failed(fullpath,
+                "Season Folder Name does not meet spec")
+    else:
+        folder_seasonnum = res.group(2)
 
-	if seriesname.endswith(', The'):
-		seriesname = "The %s" % seriesname[:-5]
+    # check filename
+    res = tv.match(filename)
+    if not res:
+        return failed(fullpath,"Filename does not meet spec")
 
-	# check name on season folder
-	try:
-		seasonnum = re.match(r'Season ([0-9]{2})',seasondir).group(1)
-	except AttributeError, e:
-		return failed(fullpath,"Season Folder Name does not meet spec")
-		
-	# check filename
-	match = re.match(r'(.*) - ([0-9]{1,2}(x([0-9]{2,3}))+)( - .*)?\.[^.]+$',filename)
-	if not match:
-		return failed(fullpath,"Filename does not meet spec")
-	
-	# compare file series name and dir series name
-	if seriesname != match.group(1):
-		return failed(fullpath, "Series Names do not match")
+    file_seriesname = res.group(1)
+    file_seasonnum = int(res.group(2))
 
-	# compare file season number and dir season number
-	if int(match.group(2).split('x')[0]) != int(seasonnum):
-			return failed(fullpath,"File and folder Season Number do not match")
+    # compare file series name and dir series name
+    if folder_seriesname != file_seriesname:
+        return failed(fullpath, "Series Names do not match")
 
-	if debug:
-		return passed(fullpath)
-	else:
-		return 0
+    # compare file season number and dir season number
+    if folder_season != 'Specials' \
+        and int(file_seasonnum) != int(folder_seasonnum):
+            return failed(fullpath,"File (%s) and folder " \
+                    "Season (%s) Number do not match" %
+                    (file_seasonnum,folder_seasonnum) )
+
+    return 0
 
 if __name__ == "__main__":
-	import argparse
+    import argparse
 
-	parser = argparse.ArgumentParser(description='Process command line arguments.')
+    parser = argparse.ArgumentParser(
+                    description='Process command line arguments.')
 
-	parser.add_argument('--debug',action='store_true',dest='debug',default=False,
-		help='Enable debug mode')
-	parser.add_argument('directory',help='Directory to validate')
+    parser.add_argument('directory',help='Directory to validate')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	path = args.directory
+    path = args.directory
 
-	if not os.path.exists(path):
-		print "Path does not exist: %s" % path
-		sys.exit(1)
+    if not os.path.exists(path):
+        print "Path does not exist: %s" % path
+        sys.exit(1)
 
-	if os.path.isfile(path):
-		sys.exit(processTV(path))
+    if os.path.isfile(path):
+        sys.exit(processTV(path))
 
-	for (d, folders, files) in os.walk(os.path.abspath(path)):
-		files = sorted(files)
-		for i in files:
-			processTV("%s/%s" % (d,i),args.debug)
+    for (d, folders, files) in os.walk(os.path.abspath(path)):
+        files = sorted(files)
+        for i in files:
+            processTV("%s/%s" % (d,i))
+
