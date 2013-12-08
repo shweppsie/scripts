cb6f5852793784741e7b0c5660633a5e2c9198a9
Nathan Overall <git@shweppsie.com>
2013-12-09 01:14:29 +1300

    Add handy tv show moving script

---
+++ bin/tvmove.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for file in "$@"
+do
+    name="`basename "${file}"`"
+    
+    if echo $name | egrep -q '.* - [0-9]*x[0-9]* - .*';
+    then
+    	# Get the show name
+		show=`echo ${name} | sed 's/\(.*\) - [0-9]*x[0-9]* - .*/\1/g'`
+		
+		# Put the at the end of the show name
+		show=`echo ${show} | sed 's/The \(.*\)/\1, The/g'`
+
+		# Get the season number
+    	season=`echo ${name} | sed 's/.* - \([0-9]*\)x[0-9]* - .*/\1/g'`
+
+		# Pad the season number to 2 digits
+    	season=`printf "%02d\n" $season`
+
+    	echo "${name} -> ${show}/Season ${season}/"
+    	mv -i "${file}" "/stuff/shared/videos/tv/${show}/Season ${season}/"
+	else
+		echo "Name does not match regex: ${name}"
+    fi
+done
