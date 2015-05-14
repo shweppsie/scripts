2b02d4c840dbf1237e74a3c5b9506db8b40dc411
Nathan Overall <git@shweppsie.com>
2015-05-14 20:00:10 +1200

    Remove script

--- video/fix_season_numbers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ls | grep '^[sS]eason [0-9]$' | while read line
-do
-	newname=`echo "${line}" | sed 's/[Ss]eason \([0-9]\)/Season 0\1/'`
-	echo "${line} -> ${newname}"
-	mv "${line}" "${newname}"
-done
